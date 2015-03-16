6677bbe774f6b7090e9b4aa407b96d60989fcd7f
sissoux <alexis.damiens59@gmail.com>
2015-03-16 18:00:16 +0100

    State mode creation
    Creation of State ==> Flash, chainedflash, static, colorfading, randomcolor, blackout.
    
    Not sure of utility of all these modes.

--- LED.h
+++ LED.h
@@ -6,16 +6,33 @@
 #define  TF 3
 #define  ACTIVE 4
 #define  TRIG 5
-
+  
+  typedef enum{           //Goal of this enum is to define different states in which the pixel can be. Normally state should be the same for all Pixels.
+    FLASH,                //This state is used to generate random duration flash on random LED
+    CHAINEDFLASH,         //This state is used to generate a random duration flash serie on all the strip length specifying start and direction
+    STATIC,               //This state is used to place LED in a static color
+    COLORFADING,          //This state generate a fading from one color to another
+    RANDOMCOLOR,          //This state generate random colors at random durations and fade between them
+    BLACKOUT              //This turn off the LED
+  }pixelState;
+  
 
 class LED
 {
-  
   public:
   LED();
-  LED(unsigned int, unsigned int);
+  LED(unsigned int ID, unsigned int Tfall);
+  
+  pixelState State = BLACKOUT;
+  pixelState PreviousState = BLACKOUT;
   
   void flash();
+  void chainedFlash();
+  void static();
+  void colorFading();
+  void randomColor();
+  void blackout();
+  void update();
   
   unsigned int Tr = 1;
   unsigned int Ts = 0;
